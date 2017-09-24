658d7638a480eb0f4d1e0401f68c1a5652f10ecf
max <max@Coding>
2017-09-24 13:45:07 +0200

    project fixes

--- AdvancedTokenField-root/AdvancedTokenField-addon/src/main/java/com/fo0/advancedtokenfield/listeners/TokenNewItemListener.java
+++
@@ -1,9 +0,0 @@
-package com.fo0.advancedtokenfield.listeners;
-
-import com.fo0.advancedtokenfield.main.Token;
-
-public interface TokenNewItemListener {
-
-	Token action(String token);
-
-}
--- AdvancedTokenField-root/AdvancedTokenField-addon/src/main/java/com/fo0/advancedtokenfield/listeners/TokenRemoveListener.java
+++
@@ -1,9 +0,0 @@
-package com.fo0.advancedtokenfield.listeners;
-
-import com.fo0.advancedtokenfield.events.TokenRemoveEvent;
-
-public interface TokenRemoveListener {
-
-	TokenRemoveEvent action(TokenRemoveEvent event);
-
-}
